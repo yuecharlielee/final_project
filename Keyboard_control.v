b772d0f0761ce74007d8501694467da463e7eee8
110062172 <yuecharlie@gapp.nthu.edu.tw>
2024-01-11 19:31:05 +0800

    update keyboard

--- Keyboard_control.v
+++ Keyboard_control.v
@@ -8,6 +8,7 @@ module Keyboard_control(clk, rst, key_down, last_change, been_ready, falling, le
     reg next_falling, next_left, next_right;
     //keyboard input
     parameter [8:0] ENTER_CODES = 9'b0_0101_1010; // enter => 5A
+    parameter [8:0] ENTER_CODES_re = 9'b0_0101_1011;
     parameter [8:0] KEY_CODES_A = 9'b0_0001_1100; // A => 1C
     parameter [8:0] KEY_CODES_D = 9'b0_0010_0011; // D => 23
     parameter [8:0] KEY_CODES_S = 9'b0_0001_1011;
@@ -68,40 +69,25 @@ module Keyboard_control(clk, rst, key_down, last_change, been_ready, falling, le
         //     next_left = 1'b0;
         //     next_right = 1'b0;
         // end
-        if(been_ready && key_down[last_change] == 1'b1) begin
-            if(last_change == ENTER_CODES) begin
-                next_falling = 1'b1;
-                next_left = left;
-                next_right = right;
-            end
-            else begin
-                // if(last_change == KEY_CODES_F) begin
-                //     next_left = 1'b1;
-                //     next_falling = falling;
-                //     next_right = right;
-                // end
-                // else begin
-                //     if(last_change == KEY_CODES_D) begin
-                //         next_right = 1'b1;
-                //         next_falling = falling;
-                //         next_left = left;
-                //     end
-                //     else begin
-                //         next_right = right;
-                //         next_falling = falling;
-                //         next_left = left;
-                //     end
-                // end
-                next_right = right;
-                next_falling = falling;
-                next_left = left;
-            end
+        if(key_down[ENTER_CODES] == 1'b1) begin
+            next_falling = 1'b1;
+        end
+        else begin
+            next_falling = 1'b0;
+        end
 
+        if(key_down[KEY_CODES_A] == 1'b1) begin
+            next_left = 1'b1;
+        end
+        else begin
+            next_left = 1'b0;
+        end
+
+        if(key_down[KEY_CODES_D] == 1'b1) begin
+            next_right = 1'b1;
         end
         else begin
             next_right = 1'b0;
-            next_falling = 1'b0;
-            next_left = 1'b0;
         end
     end
 
